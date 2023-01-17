91e1e15d86bce1b86d96854632edfdbfb9869858
Eric Alex Neuhaus <101681482+ericneuhaus@users.noreply.github.com>
2023-01-17 20:52:15 -0300

    Segundo Commit

--- calculadora.py
+++ calculadora.py
@@ -2,15 +2,15 @@ import os #Usando apenas para limpar a tela
 
 while True:
     os.system("cls")
-    str_num1 = input("Digite o primeiro número: ")
+    str_num1 = input("Digite o primeiro número: ") #Solicita primeiro número
 
     if str_num1.isnumeric():
-        int_num1 = int(str_num1)
-        str_num2 = input("Digite o segundo número: ")
+        int_num1 = int(str_num1) #Converte a entrada do usuário que é do tipo "str" para "int"
+        str_num2 = input("Digite o segundo número: ") #Solicita o segundo número
 
         if str_num2.isnumeric():
-            int_num2 = int(str_num2)
-            operador = input("digite o operador: ")
+            int_num2 = int(str_num2) #Converte a entrada do usuário que é do tipo "str" para "int"
+            operador = input("digite o operador: ") #Solicita o operador
             os.system("cls")
 
             if operador == "+":
